049ac1fe5def199239e9d5eb0cbda780220faaf6
bassvillain <gantold@hotmail.com>
2014-01-23 15:20:41 +0000

    Update boombot.js

--- Js/boombot.js
+++ Js/boombot.js
@@ -96,7 +96,7 @@ boombot.filters.beggerWords = ["fanme","fan me","fan4fan","fan 4 fan","fan pls",
 
 
 var announcements = [
-""
+"Hey man, we're doing a jazz session! Go pick your favorite jazzy hip hop tracks or instrumentals and have fun!"
 ];
 
 
