80db24a2936e4fac58ed503b19da96f5050d89a2
Tawi T Jordan <aiseiab05@yahoo.com>
2014-02-14 15:48:34 -0800

    Update boombot.js

--- Js/boombot.js
+++ Js/boombot.js
@@ -324,7 +324,7 @@ $('#woot').click();
 API.on(API.USER_JOIN, UserJoin);
 function UserJoin(user)
 {
-var JoinMsg = ["Happy valentines @user"/*,"Sup yo! @user","Welcome to The Boombox mate! @user","Aye mate! @user","Yo @user"*/];
+var JoinMsg = ["Happy valentines @user","Welcome to The Boombox mate and happy valentines day! @user"/*,"Sup yo! @user","Aye mate! @user","Yo @user"*/];
 r = Math.floor(Math.random() * JoinMsg.length);
 API.sendChat(JoinMsg[r].replace("user", user.username) + " ~ Read the rules and have fun!");
 }
